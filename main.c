620aa5034c8fa3b0268982d06ff13b3a39caa3f9
Gabriel França <32433878+GabrielFOliveira@users.noreply.github.com>
2021-06-10 20:12:01 -0300

    removendo comentario

--- main.c
+++ main.c
@@ -57,42 +57,7 @@ Celula getarqcelula(char* line){
   return(a);
 }
 
-/*void main_seq(){
-  Lista lista;
-  new_lista(&lista);
-  Lista_l lista_l;
-  new_lista_l(&lista_l);
-  add_lista_l(&lista_l,&lista);
-  FILE *pont_arq;
-  char line[1024];
-  pont_arq = fopen("dadosteste.csv", "r");
-  fgets(line, 1024, pont_arq);
-  char* tmp = strdup(line);
-  add_lista(&lista,getarqcelula(tmp));
-  print_lista(&lista);
-  while (fgets(line, 1024, pont_arq)){
-    tmp = strdup(line);
-    printf("\n\n\n Teste: %s == %s \n\n\n", lista_l.listas[lista_l.n-1].celulas[0].municipio, getfield(tmp,1));
-    printf("%d \n",(lista_l.n));
-    tmp = strdup(line);
-    if(lista_l.listas[lista_l.n-1].celulas[0].municipio==getfield(tmp,1)){
-      add_lista(&lista, getarqcelula(line));
-    }
-    else{
-      new_lista(&lista_l.listas[lista_l.n]);
-      add_lista_l(&lista_l,&lista);
-      add_lista(&lista, getarqcelula(line));
-    }
-
-
-  }
-  add_lista_l(&lista_l,&lista);
-  print_lista_l(&lista_l);
-
-
-}*/
-
-void main_seq(){
+void carrega(){
   Lista lista[100];
   int n=0;
   for(int i=0;i<100;i++){
@@ -108,31 +73,26 @@ void main_seq(){
   n++;
   while (fgets(line, 1024, pont_arq)){
     tmp = strdup(line);
-    printf("\n\n\n Teste: %s == %s \n\n\n", lista[n-1].celulas->municipio, getfield(tmp,1));
-    printf("%d \n",n);
-    tmp = strdup(line);
-    if(strcmp(&lista[n-1].celulas->municipio,getfield(tmp,1))==0){
+    if(strcmp(lista[n-1].celulas->municipio,getfield(tmp,1))==0){
       add_lista(&lista[n-1], getarqcelula(line));
     }
     else{
       add_lista(&lista[n], getarqcelula(line));
       n++;
     }
-
-
   }
   for(int i=0;i<n;i++){
     printf("%s\n",lista[i].celulas->municipio);
   }
-
 }
 
 int main(){
   int tipo;
+  carrega();
 	scanf("%d", &tipo);
 	switch ( tipo ) {
     case 1 :
-      main_seq();
+      //main_seq();
     break;
     case 2 :
       //main_bin();
